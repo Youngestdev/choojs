ed954b7caca0628b0502c7e9834391d62073356d
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-08-17 20:42:57 +0100

    :tada:

--- views/404.js
+++ views/404.js
@@ -7,7 +7,7 @@ module.exports = view
 function view (state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
   return html`
-    <body class="sans-serif pa3">
+    <body class="paper container">
       <h1>Route not found.</h1>
       <a class="pt2" href="/">Back to main.</a>
     </body>
